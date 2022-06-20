0271aa494af29a5e51c4cebf6ab7b8a57fde29b8
12emin34 <macanovic.emin@gmail.com>
2022-06-20 10:42:21 +0200

    Initial Commit

---
+++ src/main/java/me/_12emin34/mouseclickswap/MouseClickSwap.java
@@ -0,0 +1,11 @@
+package me._12emin34.mouseclickswap;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MouseClickSwap implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/_12emin34/mouseclickswap/client/MouseClickSwapClient.java
@@ -0,0 +1,39 @@
+package me._12emin34.mouseclickswap.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+@Environment(EnvType.CLIENT)
+public class MouseClickSwapClient implements ClientModInitializer {
+    private static KeyBinding swapKeybinding;
+
+    @Override
+    public void onInitializeClient() {
+        swapKeybinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.mouseclickswap.swap",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_R,
+                "category.mouseclickswap.general"
+        ));
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            GameOptions options = client.options;
+            KeyBinding attackKeybinding = options.attackKey;
+            KeyBinding useKeybinding = options.useKey;
+            while (swapKeybinding.wasPressed()) {
+                InputUtil.Key attackKey = KeyBindingHelper.getBoundKeyOf(attackKeybinding);
+                InputUtil.Key useKey = KeyBindingHelper.getBoundKeyOf(useKeybinding);
+
+                options.setKeyCode(attackKeybinding, useKey);
+                options.setKeyCode(useKeybinding,attackKey);
+                KeyBinding.updateKeysByCode();
+            }
+        });
+    }
+}
