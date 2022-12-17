09155ad69920c9304f68649d25a2199984acae2d
12emin34 <macanovic.emin@gmail.com>
2022-12-17 14:33:03 +0100

    fix: config should now actually work

--- src/main/java/me/_12emin34/mouseclickswap/config/ModConfig.java
+++ src/main/java/me/_12emin34/mouseclickswap/config/ModConfig.java
@@ -1,5 +1,6 @@
 package me._12emin34.mouseclickswap.config;
 
+import com.google.common.collect.Lists;
 import eu.midnightdust.lib.config.MidnightConfig;
 
 import java.util.List;
@@ -8,5 +9,5 @@ public class ModConfig extends MidnightConfig {
     @Entry(name = "Enable auto-swap")
     public static boolean enableAutoSwap = false;
     @Entry(name = "Auto-swap items")
-    public static List<String> autoSwapItems = List.of("minecraft:diamond_sword", "minecraft:diamond_axe");
+    public static List<String> autoSwapItems = Lists.newArrayList("minecraft:diamond_sword", "minecraft:diamond_axe");
 }
