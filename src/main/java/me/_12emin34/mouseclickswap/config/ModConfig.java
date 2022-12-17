3ecbc6431831e26924ff81e76bde3a6608ddd8e2
12emin34 <macanovic.emin@gmail.com>
2022-12-17 16:25:56 +0100

    fix: misc fixes and default config

--- src/main/java/me/_12emin34/mouseclickswap/config/ModConfig.java
+++ src/main/java/me/_12emin34/mouseclickswap/config/ModConfig.java
@@ -9,5 +9,18 @@ public class ModConfig extends MidnightConfig {
     @Entry(name = "Enable auto-swap")
     public static boolean enableAutoSwap = false;
     @Entry(name = "Auto-swap items")
-    public static List<String> autoSwapItems = Lists.newArrayList("minecraft:diamond_sword", "minecraft:diamond_axe");
+    public static List<String> autoSwapItems = Lists.newArrayList(
+            "minecraft:wooden_sword",
+            "minecraft:stone_sword",
+            "minecraft:iron_sword",
+            "minecraft:diamond_sword",
+            "minecraft:golden_sword",
+            "minecraft:netherite_sword",
+            "minecraft:wooden_axe",
+            "minecraft:stone_axe",
+            "minecraft:iron_axe",
+            "minecraft:diamond_axe",
+            "minecraft:golden_axe",
+            "minecraft:netherite_axe"
+    );
 }
